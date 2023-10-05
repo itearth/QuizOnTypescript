b34e895869c6a410bf13624dadbdd6f11fc7c14a
itearth <awani0005@gmail.com>
2023-10-05 10:28:10 +0530

    Quiz app Navigation problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Application from '../src/components/pages/Quiz/Application';
+import { Route, Routes } from 'react-router-dom';
+import Application from './components/pages/Quiz/Application';
+import HomePage from './components/pages/HomePage/HomePage';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Application />} />
-        
+    
+      <Routes>  
+      <Route path="/" element={<HomePage />} />
+      <Route path="/application" element={<Application />} /> 
       </Routes>
-    </Router>
+   
   );
 };
 
---
+++ src/components/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomePage.module.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <div className="centered-container">
+        <button className="button">Take Quiz</button>
+        <button className="button">See Weather</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
